961bb92db172b7ddb11147f6ef8aface1a0afa23
tyland-ti <triview@naver.com>
2022-02-02 20:40:31 +0900

    first commit

---
+++ src/test/java/jpabook/jpashop/repossitory/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.repossitory;
+
+import jpabook.jpashop.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() {
+        //given
+        Member member = new Member();
+        member.setUsername("name1");
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        assertEquals(member.getId(), findMember.getId());
+        assertEquals(member.getUsername(), findMember.getUsername());
+
+    }
+}
