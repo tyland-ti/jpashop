a2532e1a83a65edb6ae987c9f68f3edbab97a1f0
tyland-ti <triview@naver.com>
2022-02-04 00:15:30 +0900

    define entity

--- src/main/java/jpabook/jpashop/config/SwaggerConfig.java
+++ src/main/java/jpabook/jpashop/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -14,21 +13,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-        @Bean
-        public Docket api() {
-                return new Docket(DocumentationType.SWAGGER_2)
-                        .select()
-                        .paths(PathSelectors.any())
-                        .build()
-                        .apiInfo(apiInfo())
-                        .useDefaultResponseMessages(false); // Response 응답 메시지 디폴트값 적용 X
-        }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false); // Response 응답 메시지 디폴트값 적용 X
+    }
 
-        private ApiInfo apiInfo() {
-                return new ApiInfoBuilder()
-                        .title("API")
-                        .description("스프링부트 JPA 샘플")
-                        .version("v1")
-                        .build();
-        }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API")
+                .description("스프링부트 JPA 샘플")
+                .version("v1")
+                .build();
+    }
 }
--- src/test/java/jpabook/jpashop/repossitory/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repossitory/MemberRepositoryTest.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop.repossitory;
 
-import jpabook.jpashop.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
